97cf75ef4f06fd892dee460225062b48c405becf
mp <marco.panzeri9@gmail.com>
2021-03-01 17:30:29 +0100

    add: update copyright

--- src/.vuepress/theme/components/Footer.vue
+++ src/.vuepress/theme/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
     <footer>
-      <span class="copyright--notice">Copyright 2019 Databiz Group S.r.l. - Partita IVA/C.F.:04268680263</span>
+      <span class="copyright--notice">Copyright 2021 Fortitude Group S.r.l. All rights reserved.  C.F. / P.IVA: 04268680263</span>
     </footer>
 </template>
