ba4f2d9f70405fa033cf7c33cd3704d5f86748dd
mp <marco.panzeri9@gmail.com>
2021-03-01 17:51:21 +0100

    fix: responsive copyright

--- src/.vuepress/theme/components/Footer.vue
+++ src/.vuepress/theme/components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
     <footer>
-      <span class="copyright--notice">Copyright 2021 Fortitude Group S.r.l. All rights reserved.  C.F. / P.IVA: 04268680263</span>
+      <span class="copyright--notice">Copyright 2021 Fortitude Group S.r.l. All rights reserved.
+        <span>C.F. / P.IVA: 04268680263</span></span>
     </footer>
 </template>
