1bff7eb425e24343602c4d6496df7b112d3fad27
marco Panzeri <marco.panzeri9@gmail.com>
2021-04-15 08:57:36 +0200

    add: iubenda cookie notice (#40)

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,6 +3,10 @@ module.exports = {
   description: 'Starting January 2021, Databiz Group has become Fortitude Group. We didn’t change just our naming and logo: we started a new phase of our story.  Discover our new brand-identity.',
 
   head: [
+    ['script', {}, `var _iub = _iub || [];
+    _iub.csConfiguration = {"whitelabel":false,"lang":"en","siteId":1410308,"cookiePolicyId":15812422, "banner":{ "slideDown":false,"applyStyles":false,"position":"bottom" }};
+    `],
+    ['script', { src: '//cdn.iubenda.com/cs/iubenda_cs.js'}],
     ['link', { rel: 'icon', href: '/logos/dbz.png' }],
     ['link', { rel: 'manifest', href: '/app.webmanifest' }],
     ['meta', { name: 'theme-color', content: '#0072bc' }],
